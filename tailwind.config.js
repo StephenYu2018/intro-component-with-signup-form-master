1639391c54480bed9178297796dbc60495cc334f
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-28 13:44:23 -0400

    Add colors and font family to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors: {
+      red: "hsl(0, 100%, 74%)",
+      green: "hsl(154, 59%, 51%)",
+      blue: "hsl(248, 32%, 49%)",
+      "dark-blue": "hsl(249, 10%, 26%)",
+      "grayish-blue": "hsl(246, 25%, 77%)",
+    },
+    fontFamily: {
+      poppins: ["'Poppins'", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
