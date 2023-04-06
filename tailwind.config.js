a293e5e67ee1e95bce83d383f2ec7d47997557aa
StephenYu2018 <stephenyu8021@gmail.com>
2023-04-06 11:27:28 -0400

    add client-side validation logic & styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./index.html",
+    "./validate.js"
+  ],
   theme: {
     colors: {
       red: "hsl(0, 100%, 74%)",
+      "desaturated-red": "hsl(355, 60%, 65%)",
       green: "hsl(154, 59%, 51%)",
       blue: "hsl(248, 32%, 49%)",
       "dark-blue": "hsl(249, 10%, 26%)",
---
+++ validate.js
@@ -0,0 +1,118 @@
+const signUpForm = document.querySelector('form');
+
+const firstName = document.getElementById('first-name');
+const firstNameError = document.getElementById('first-name-error');
+
+const lastName = document.getElementById('last-name');
+const lastNameError = document.getElementById('last-name-error');
+
+const email = document.getElementById('email-address');
+const emailError = document.getElementById('email-address-error');
+
+const password = document.getElementById('password');
+const passwordError = document.getElementById('password-error');
+
+const errorInputStyles = [
+  'border-desaturated-red', 'border-2', 'placeholder:text-desaturated-red'
+];
+const defaultInputStyles = ['border-grayish-blue/60',
+  'placeholder:text-dark-blue'];
+
+signUpForm.addEventListener('submit', function(event) {
+  if (isEmpty(firstName)) {
+    showInvalid({ input: firstName, error: firstNameError,
+      message: 'First name cannot be empty'
+    }, '');
+    event.preventDefault();
+  }
+
+  if (isEmpty(lastName)) {
+    showInvalid({ input: lastName, error: lastNameError,
+      message: 'Last name cannot be empty'
+    }, '');
+    event.preventDefault();
+  }
+
+  if (!isEmailValid()) {
+    showInvalid({ input: email, error: emailError,
+      message: 'Looks like this is not an email'
+    }, 'email@example/com');
+    event.preventDefault();
+  }
+
+  if (isEmpty(password)) {
+    showInvalid({ input: password, error: passwordError,
+      message: 'Password cannot be empty'
+    }, '');
+    event.preventDefault();
+  }
+});
+
+function isEmpty(input) {
+  return input.value.length === 0;
+}
+
+function showInvalid({ input, error, message }, placeholder) {
+  error.classList.remove('hidden');
+  error.textContent = message;
+
+  input.classList.remove(...defaultInputStyles);
+  input.classList.add(...errorInputStyles);
+  input.style.background = "url('./images/icon-error.svg') no-repeat right 1.5rem top 50%";
+  input.value = '';
+  input.placeholder = placeholder;
+}
+
+function isEmailValid() {
+  const emailFormat = /[\w!#\$%&'\*\+\-\/=\?\^`\{\|\}~]+(\.[\w!#\$%&'\*\+\-\/=\?\^`\{\|\}~]+)*@\d*[a-z]+\d*(\-[a-z0-9]+)*(\.\d*[a-z]+\d*(\-[a-z0-9]+)*)*(\.[a-z]+)?/mi;
+  return email.value.match(emailFormat) && areAllEmailDomainLabelsValid();
+}
+
+function areAllEmailDomainLabelsValid() {
+  const labels = email.value
+    .slice(email.value.indexOf('@'), email.value.lastIndexOf('.'))
+    .split('.');
+  
+  for (const label of labels) {
+    if (label.length > 63) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+function hideInvalid({ input, error }, placeholder) {
+  return function(event) {
+    if (error.textContent !== '') {
+      error.classList.add('hidden');
+      error.textContent = '';
+
+      input.classList.remove(...errorInputStyles);
+      input.classList.add(...defaultInputStyles);
+      input.style.background = 'none';
+      input.placeholder = placeholder;
+    }
+    event.preventDefault();
+  }
+}
+
+firstName.addEventListener('focus', hideInvalid({
+  input: firstName,
+  error: firstNameError
+}, 'First Name'));
+
+lastName.addEventListener('focus', hideInvalid({
+  input: lastName,
+  error: lastNameError,
+}, 'Last Name'));
+
+email.addEventListener('focus', hideInvalid({
+  input: email,
+  error: emailError
+}, 'Email Address'));
+
+password.addEventListener('focus', hideInvalid({
+  input: password,
+  error: passwordError
+}, 'Password'));
